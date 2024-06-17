17d5d6c8d11cbd981416f46489beea4d5929657b
nlcol3gon3 <131747232+nlcol3gon3@users.noreply.github.com>
2024-06-17 12:34:40 +0300

    Add files via upload

---
+++ organizerLogin.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+include("database.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Organizer Login</title>
+</head>
+
+<body class="organizerPage">
+    <?php include 'header.html'; ?>
+
+    <div class="wrapper">
+        <div class="form-box login">
+            <h2>ORGANIZER  LOGIN</h2>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <input type="email" name="email" required>
+                    <label>Email</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <input type="password" name="password" required>
+                    <label>Password</label>
+                </div>
+                <div class="remember-forgot">
+                    <label><input type="checkbox">Remember me</label>
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="login-register">
+                    <p style="font-size: 0.9em, position: centre, text-align: center;">Don't have an account? <a href="organizerRegistration.php">Register here.</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM eventorganizer WHERE email = '$email' AND password = '$password';";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION['organizerID'] = $row['organizerID'];
+        header('Location: organizerHomePage.php');
+        exit();
+    } else {
+        echo "<p style='color: red; text-align: center;'>Invalid email or password.</p>";
+    }
+}
+?>
